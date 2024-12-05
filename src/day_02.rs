a1fba40c750c36917247a7aecab9468839cb1330
Sam Collier <samcollier916@gmail.com>
2024-12-05 00:33:32 +0000

    day 2 part 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -1,34 +1,49 @@
 use crate::utils;
 
 fn are_levels_safe(levels: &Vec<i8>) -> bool {
-    let mut has_increasing = false;
-    let mut has_decreasing = false;
-    let mut has_valid_spacing = true;
+    let mut is_increasing = false;
+    let mut is_decreasing = false;
     for (level_idx, level) in levels.iter().enumerate() {
         if let Some(next_level) = levels.get(level_idx + 1) {
-            has_increasing = has_increasing || level < next_level;
-            has_decreasing = has_decreasing || level > next_level;
+            is_increasing = is_increasing || level < next_level;
+            is_decreasing = is_decreasing || level > next_level;
             let distance = (level - next_level).abs();
-            has_valid_spacing = has_valid_spacing && distance >= 1 && distance <= 3;
+            let has_valid_spacing = distance >= 1 && distance <= 3;
+            if (is_increasing && is_decreasing) || !has_valid_spacing {
+                return false;
+            }
         }
     }
-    (has_increasing ^ has_decreasing) && has_valid_spacing
+    true
 }
 
 pub fn day_02() -> (String, String) {
     let input = utils::fs::read_aoc_input(2);
     let input = input.split_terminator('\n');
     let mut total_safe = 0;
+    let mut total_dampened = 0;
     for line in input {
-        let level_strings = line.split_terminator(' ');
-        let levels = level_strings
+        let levels = line.split_terminator(' ');
+        let levels = levels
             .map(|level: &str| level.parse().expect("unable to parse integer!"))
             .collect::<Vec<i8>>();
 
         if are_levels_safe(&levels) {
-            total_safe += 1;
+            total_safe += 1
+        } else {
+            for index_to_remove in 0..levels.len() {
+                let mut level_clone = levels.clone();
+                level_clone.remove(index_to_remove);
+                if are_levels_safe(&level_clone) {
+                    total_dampened += 1;
+                    break;
+                }
+            }
         }
     }
 
-    (total_safe.to_string(), "".to_string())
+    (
+        total_safe.to_string(),
+        (total_safe + total_dampened).to_string(),
+    )
 }
